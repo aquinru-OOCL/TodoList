25379f74671b1844865c4da42976251031c90edb
AQUINRU <russel.aquino@oocl.com>
2021-08-18 21:53:56 +0800

    [TodoList][Russel] feat: added design in NotFound.js

--- src/features/todos/components/NotFound.js
+++ src/features/todos/components/NotFound.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { Result } from 'antd';
 
 class NotFound extends Component {
 
     render() {
         return (
-            <span>
-                <br></br><br></br>
-                Error 404: The page you're trying to visit doesn't exist.
-            </span>
+            <Result
+                status="404"
+                title="404"
+                subTitle="Sorry, the page you're trying to visit doesn't exist."
+            />
         );
     }
 }
