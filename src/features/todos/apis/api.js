9d48a38c7feccf8a9e174e240ed318d56ed5107f
AQUINRU <russel.aquino@oocl.com>
2021-08-19 18:19:57 +0800

    [TodoList][Russel] feat: connect back-end

--- src/features/todos/apis/api.js
+++ src/features/todos/apis/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://611cd50c7d273a0017e2f461.mockapi.io/'
+    //baseURL: 'https://611cd50c7d273a0017e2f461.mockapi.io/'
+    baseURL: 'http://localhost:8090',
 });
 
 export default api;
