06c71c482cd527d083a464b95cb46a21070ef307
AQUINRU <russel.aquino@oocl.com>
2021-08-19 21:44:06 +0800

    [TodoList][Russel] feat: modal for update todo text

--- src/features/todos/components/TodoItem.js
+++ src/features/todos/components/TodoItem.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch} from "react-redux";
 import { deleteTodo, updateTodo } from "../apis/todos";
 import { ToggleTodo, DeleteTodo} from "../reducers/todoSlice";
 import "../styles/TodoItem.css";
+import { Modal } from 'antd';
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
 function TodoItem(props) {
     const dispatch = useDispatch();
     const todoStatus = props.todo.done ? "done" : "";
     const todoId = props.todo.id;
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
     function handleClick() {
         updateTodo(todoId, {done: !props.todo.done}).then((response) => {
@@ -24,17 +26,34 @@ function TodoItem(props) {
         });
     }
 
+    function showModal (event) {
+        event.stopPropagation();
+        setIsModalVisible(true);
+    };
+    
+    function handleOk (event) {
+        event.stopPropagation();
+        setIsModalVisible(false);
+    };
+    
+    function handleCancel (event) {
+        event.stopPropagation();
+        setIsModalVisible(false);
+    };
+
     return (
         <div className="TodoItem">
             <ul className={`TodoItem-todo ${todoStatus}`} onClick={handleClick}>
                 <li>
                     {props.todo.text}
                     <DeleteOutlined className="close" onClick={handleDelete}></DeleteOutlined>
-                    <EditOutlined className="update" ></EditOutlined>
+                    <EditOutlined className={`update ${todoStatus}`} onClick={showModal}></EditOutlined>
+                    <Modal title="Update this todo item" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} width="40%">
+                        <span>{props.todo.text}</span>
+                    </Modal>
                 </li>
             </ul>
         </div>
-        
     );
 }
 
